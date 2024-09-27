e92acbd4312622980988dc31e6b538cc5740647b
maybeayush <155345759+maybeayush@users.noreply.github.com>
2024-09-27 17:22:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function moveRandomEl(elm) {
   elm.style.position = "absolute";
-  elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
-  elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+  elm.style.top = Math.floor(Math.random() * 90 + 8) + "%";
+  elm.style.left = Math.floor(Math.random() * 90 + 8) + "%";
 }
 
 const moveRandom = document.querySelector("#move-random");
